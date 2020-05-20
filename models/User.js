12f188ed0ae8dc9da36e3a8de5a29197321b62fe
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2020-05-21 03:16:32 +0530

    Initial setup, Auth module added

---
+++ models/User.js
@@ -0,0 +1,83 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          "Please add a valid email",
+        ],
+      },
+    role: {
+        type: String,
+        enum: ['user'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minLength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }  
+});
+
+// Encrypt password using bcrypt ( middleware )
+UserSchema.pre('save', async function(next) {
+
+    // For the sake of forgot password
+    // If the password is not modified, means it is not provided at the time of register / login
+    if(!this.isModified('password')) {
+        next();
+    }
+
+    // Always run only at the time of register / login, in case of forgot password it will not run.
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return ( Just a method)
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    // Generete Token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+    
+    // Return token without hash
+    return resetToken;
+}
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ server.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+const errorHandler = require('./middlewares/error');
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route Files
+// const bootcamps = require("./routes/bootcamps");
+// const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+// const users = require("./routes/users");
+// const reviews = require("./routes/reviews");
+// const who = require("./routes/whoAmI");
+
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// File Uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XXS attacks
+app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100  // Number of request to be allowed
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+// app.use("/api/v1/bootcamps", bootcamps);
+// app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+// app.use("/api/v1/users", users);
+// app.use("/api/v1/reviews", reviews);
+// app.use("/api/v1/whomi", who);
+
+// Custom error handler middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections 
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
